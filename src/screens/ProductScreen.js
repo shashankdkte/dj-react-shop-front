f00bdfa2f2f23d0a131e15a29c595b7706bcef07
Shashank Dhakate <shashankdkte@gmail.com>
2023-09-26 16:08:17 +0530

    Adding Product Screen

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,8 +1,90 @@
 import React from 'react'
+import { Button, Card, Col, Image, ListGroup, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import products from '../resources/products'
+import { useParams } from 'react-router-dom'
+import Rating from '../components/Rating'
 
-function ProductScreen() {
+function ProductScreen(props) {
+
+  const { id } = useParams();
+  console.log(id);
+
+  const product = products.find((product) => product._id === id);
   return (
-    <div>ProductScreen</div>
+    <div>
+      <Link to={`/`} className='btn btn-light my-3'>Go Back</Link>
+      <div>
+        <Row>
+          <Col md={6}>
+            <Image src={product && product.image}/>
+          </Col>
+          <Col md={3}>
+                                    <ListGroup variant="flush">
+                                        <ListGroup.Item>
+                                            <h3>{product.name}</h3>
+                                        </ListGroup.Item>
+
+                                        <ListGroup.Item>
+                                            <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
+                                        </ListGroup.Item>
+
+                                        <ListGroup.Item>
+                                            Price: ${product.price}
+                                        </ListGroup.Item>
+
+                                        <ListGroup.Item>
+                                            Description: {product.description}
+                                        </ListGroup.Item>
+                                    </ListGroup>
+                                </Col>
+            <Col md={3}>
+                                    <Card>
+                                        <ListGroup variant='flush'>
+                                            <ListGroup.Item>
+                                                <Row>
+                                                    <Col>Price:</Col>
+                                                    <Col>
+                                                        <strong>${product.price}</strong>
+                                                    </Col>
+                                                </Row>
+                                            </ListGroup.Item>
+                                            <ListGroup.Item>
+                                                <Row>
+                                                    <Col>Status:</Col>
+                                                    <Col>
+                                                        {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                                                    </Col>
+                                                </Row>
+                                            </ListGroup.Item>
+
+                                            {product.countInStock > 0 && (
+                                                <ListGroup.Item>
+                                                    <Row>
+                                                        <Col>Qty</Col>
+                                                        <Col xs='auto' className='my-1'>
+                                                            
+                                                        </Col>
+                                                    </Row>
+                                                </ListGroup.Item>
+                                            )}
+
+
+                                            <ListGroup.Item>
+                                                <Button
+                                                    
+                                                    className='btn-block'
+                                                    disabled={product.countInStock == 0}
+                                                    type='button'>
+                                                    Add to Cart
+                                                </Button>
+                                            </ListGroup.Item>
+                                        </ListGroup>
+                                    </Card>
+                                </Col>
+                            </Row>
+      </div>
+    </div>
   )
 }
 
