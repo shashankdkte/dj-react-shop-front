ab9685b996d4ed6f126858befa4c1ea456e44729
Shashank Dhakate <shashankdkte@gmail.com>
2023-10-29 20:59:39 +0530

    User List Screen

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -15,7 +15,11 @@ import {
   USER_UPDATE_PROFILE_SUCCESS,
   USER_UPDATE_PROFILE_REQUEST,
   USER_DETAILS_RESET,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
+  USER_LIST_FAIL,
 } from "../constants/userConstants";
+import { ORDER_LIST_MY_RESET } from '../constants/orderConstants';
 
 export const login = (email, password) => async (dispatch) => {
   try
@@ -59,7 +63,10 @@ export const logout = () => (dispatch) => {
   console.log("dith")
    localStorage.removeItem('userInfo')
   dispatch({ type: USER_LOGOUT })
-  dispatch({type:USER_DETAILS_RESET})
+    dispatch({ type: USER_DETAILS_RESET })
+    dispatch({ type: ORDER_LIST_MY_RESET })
+    // dispatch({ type: Order_list })
+    
 }
 
 
@@ -181,6 +188,40 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: USER_UPDATE_PROFILE_FAIL,
+            payload: error.response && error.response.data.detail
+                ? error.response.data.detail
+                : error.message,
+        })
+    }
+}
+
+export const listUsers = () => async (dispatch, getState) => {
+    try
+    {
+        dispatch({
+            type:USER_LIST_REQUEST
+        })
+
+        const { userLogin: { userInfo } } = getState();
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization:`Bearer ${userInfo.token}`
+            }
+        }
+
+        const { data } = await axios.get(`/api/users/`, config);
+
+        dispatch({
+            type: USER_LIST_SUCCESS,
+            payload:data
+        })
+    }
+    catch (error)
+    {
+        dispatch({
+            type: USER_LIST_FAIL,
             payload: error.response && error.response.data.detail
                 ? error.response.data.detail
                 : error.message,
--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productDetailReducer, productListReducer } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
-import { userDetailsReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer } from "./reducers/userReducers";
+import { userDetailsReducer, userLoginReducer, userRegisterReducer, userUpdateProfileReducer,userListReducer } from "./reducers/userReducers";
 import {
   orderCreateReducer, orderDetailsReducer,
   orderListMyReducer,
@@ -20,6 +20,7 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  userList:userListReducer,
 
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
