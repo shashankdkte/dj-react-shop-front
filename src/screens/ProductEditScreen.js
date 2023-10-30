7e87541ab79b9893f7d99be9e6c4f6daecf4aefe
Shashank Dhakate <shashankdkte@gmail.com>
2023-10-31 01:42:52 +0530

    Upload Product Image

--- src/screens/ProductEditScreen.js
+++ src/screens/ProductEditScreen.js
@@ -7,6 +7,7 @@ import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { Button, Form } from "react-bootstrap";
 import { PRODUCT_UPDATE_RESET } from "../constants/productConstants";
+import axios from "axios";
 
 function ProductEditScreen() {
 
@@ -18,7 +19,7 @@ function ProductEditScreen() {
     const [category, setCategory] = useState('')
     const [countInStock, setCountInStock] = useState(0)
   const [description, setDescription] = useState('')
-  
+    const [uploading, setUploading] = useState(false)
 
   const dispatch = useDispatch();
   const navigate = useNavigate()
@@ -66,6 +67,35 @@ function ProductEditScreen() {
             countInStock,
             description
         }))
+  }
+  
+
+
+  const uploadFileHandler = async (e) => {
+        const file = e.target.files[0]
+        const formData = new FormData()
+
+        formData.append('image', file)
+        formData.append('product_id', productId)
+
+        setUploading(true)
+
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+
+            const { data } = await axios.post('/api/products/upload/', formData, config)
+
+
+            setImage(data)
+            setUploading(false)
+
+        } catch (error) {
+            setUploading(false)
+        }
     }
   return (<div>
      <Link to='/admin/productlist'>
@@ -119,7 +149,7 @@ function ProductEditScreen() {
                                     id='image-file'
                                     label='Choose File'
                                     custom
-                onChange={() => { }}
+                onChange={uploadFileHandler}
                 type="file"
                                 ></Form.Control>
                                 
