bfee3e80af06f1638704422d090fc88823dcfe29
Shashank Dhakate <shashankdkte@gmail.com>
2023-10-31 01:22:02 +0530

    Edit Screen Partial

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import OrderScreen from "./screens/OrderScreen";
 import UserListScreen from "./screens/UserListScreen";
 import UserEditScreen from "./screens/UserEditScreen";
 import ProductListScreen from "./screens/ProductListScreen";
+import ProductEditScreen from "./screens/ProductEditScreen";
 
 function App() {
   return (
@@ -22,8 +23,8 @@ function App() {
       <Header />
     <main className="App">
           <Container>
-            <Routes>
-              <Route  path="/" element={<HomeScreen />}  exact/>
+            <Routes> 
+              <Route  path="/" element={<HomeScreen/>}  exact/>
               <Route  path="/login" element={<LoginScreen />}  />
               <Route  path="/register" element={<RegisterScreen />}  />
               <Route  path="/profile" element={<ProfileScreen />}  />
@@ -37,7 +38,8 @@ function App() {
               <Route path="/admin/userlist" element={<UserListScreen />}/>
             <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
             
-            <Route path="/admin/productlist" element={<ProductListScreen/>}/>
+            <Route path="/admin/productlist" element={<ProductListScreen />} />
+             <Route path='/admin/product/:id/edit' element={<ProductEditScreen />} />
           </Routes>
           </Container>
      </main>
